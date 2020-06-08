5266adc30ed04c56ac28620be3791c5dd599f7ed
Katty Barroso <kattybarroso1321@gmail.com>
2020-06-08 13:47:05 -0700

    message

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,11 @@ const bcrypt = require('bcryptjs');
 router.post('/signup', async (req, res) => {
     const {username, password, email} = req.body;
 
+
+///hash password
+const salt = await bcrypt.genSalt(10);
+const hashPassword = await bcrypt.hash(password, salt);
+
 ///check if username and email exist
     UserModel.findOne({username})
     .then( response => { 
@@ -12,30 +17,29 @@ router.post('/signup', async (req, res) => {
             res.send({message:"username already exist"})
         }else{
             UserModel.findOne({email})
-            .then(response => { if(response){ res.send({message:"email already exist"});} })
+            .then(response => { 
+                if(response){ 
+                    res.send({message:"email already exist"});
+                }else{
+                      ///create the new user
+                        const user = new UserModel({
+                            username,
+                            password:hashPassword,
+                            email
+                        });
+
+                        user.save()
+                        .then(response => {
+                            res.send({message:"user was sucessfully created"});
+                        })
+                        .catch(err => {
+                            res.status(400).send({message:"something when wrong"});
+                        })  
+                }
+            
+            })
         }
     });
-
-    ///hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-
-    ///create the new user
-    const user = new UserModel({
-        username,
-        password:hashPassword,
-        email
-    });
-
-    user.save()
-    .then(response => {
-        res.send(response);
-    })
-    .catch(err => {
-        res.status(400).send({message:"something when wrong"});
-    })                
-   
-
 });
 
 
