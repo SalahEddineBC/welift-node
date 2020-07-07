4cf9ec83255563f20ae30e87929abf7d0600f9e5
Katty Barroso <kattybarroso1321@gmail.com>
2020-07-07 13:55:23 -0700

    main

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const path = require('path');
 
 //import routes
 const authRoute = require('./routes/auth');
 const mainRoute = require('./routes/workout');
 const userRoute = require('./routes/user');
+
+
 
 
 var cors = require("cors");
@@ -17,7 +20,7 @@ dotenv.config();
 
 
 ///connect to DB
-mongoose.connect(process.env.DB_CONNECT , 
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_CONNECT  , 
     { useNewUrlParser: true },
     () => {
     console.log('connected to db')
@@ -26,16 +29,19 @@ mongoose.connect(process.env.DB_CONNECT ,
 ///body parser
 app.use(express.json());
 
-
-
-
-
-
-
 ///Route middleware
 app.use('/workouts', mainRoute);
 app.use('/auth', authRoute);
 app.use('/user', userRoute);
+
+
+///heroku
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('../frontend/build'));
+    app.get('*', (req, res)  => {
+        res.sendfile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 
 
