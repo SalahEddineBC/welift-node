d56339221f2822809ac000a1673d4400fbd61622
Katty Barroso <kattybarroso1321@gmail.com>
2020-07-07 15:57:02 -0700

    modify path

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use('/user', userRoute);
 ///heroku
     app.use(express.static('./frontend/build'));
     app.get('*', (req, res)  => {
-        res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+        res.sendFile('/frontend/build/index.html');
     })
 
 
