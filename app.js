ba19a7013e74c32806e084d0a0214ec4136117f4
Katty Barroso <kattybarroso1321@gmail.com>
2020-07-07 15:35:54 -0700

    env

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ dotenv.config();
 
 
 ///connect to DB
-mongoose.connect( process.env.DB_CONNECT  , 
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_CONNECT  , 
     { useNewUrlParser: true, useUnifiedTopology: true},
     () => {
     console.log('connected to db')
